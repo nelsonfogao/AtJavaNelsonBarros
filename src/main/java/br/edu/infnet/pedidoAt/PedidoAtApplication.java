0c6104ab56a8627b394508ee1dc2db9eee51878a
nelsonbarros <nelson.barros@carambola.vc>
2021-12-08 01:57:12 -0300

    first commit

---
+++ src/main/java/br/edu/infnet/pedidoAt/PedidoAtApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.pedidoAt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoAtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoAtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/pedidoAt/PedidoAtApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.pedidoAt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoAtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
